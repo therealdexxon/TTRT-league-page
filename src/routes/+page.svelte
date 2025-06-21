7f1f59250eb0a55b5daddea2b0054fbf82ab5448
therealdexxon <95049453+therealdexxon@users.noreply.github.com>
2025-06-21 11:15:59 -0400

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,197 +1,252 @@
 <script>
-	import LinearProgress from '@smui/linear-progress';
-	import { getNflState, leagueName, getAwards, getLeagueTeamManagers, homepageText, managers, gotoManager, enableBlog, waitForAll } from '$lib/utils/helper';
-	import { Transactions, PowerRankings, HomePost} from '$lib/components';
-	import { getAvatarFromTeamManagers, getTeamFromTeamManagers } from '$lib/utils/helperFunctions/universalFunctions';
+  import LinearProgress from '@smui/linear-progress';
+  import { getNflState, leagueName, getAwards, getLeagueTeamManagers, homepageText, managers, gotoManager, enableBlog, waitForAll } from '$lib/utils/helper';
+  import { Transactions, PowerRankings, HomePost } from '$lib/components';
+  import { getAvatarFromTeamManagers, getTeamFromTeamManagers } from '$lib/utils/helperFunctions/universalFunctions';
+  import { onMount } from 'svelte';
 
-    const nflState = getNflState();
-    const podiumsData = getAwards();
-    const leagueTeamManagersData = getLeagueTeamManagers();
+  // NFL State, Awards, and League Team Managers Data
+  const nflState = getNflState();
+  const podiumsData = getAwards();
+  const leagueTeamManagersData = getLeagueTeamManagers();
+
+  // Countdown Timer Variables
+  let countdownText = '';
+  const countdownDate = new Date('2025-08-31T00:00:00-05:00'); // Set to the draft date (Central Time)
+
+  // Countdown Update Function
+  const updateCountdown = () => {
+    const now = new Date().getTime();
+    const distance = countdownDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    if (distance < 0) {
+      countdownText = "The Draft is Live!";
+    } else {
+      countdownText = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+    }
+  };
+
+  // Start Countdown on Mount
+  onMount(() => {
+    updateCountdown(); // Initial call
+    const interval = setInterval(updateCountdown, 1000); // Update every second
+
+    return () => clearInterval(interval); // Cleanup on unmount
+  });
 </script>
 
 <style>
-    #home {
-        display: flex;
-        flex-wrap: nowrap;
-        position: relative;
-        overflow-y: hidden;
-        z-index: 1;
-    }
+  #home {
+    display: flex;
+    flex-wrap: nowrap;
+    position: relative;
+    overflow-y: hidden;
+    z-index: 1;
+  }
 
-    #main {
-        flex-grow: 1;
-        min-width: 320px;
-        margin: 0 auto;
-        padding: 60px 0;
-    }
+  #main {
+    flex-grow: 1;
+    min-width: 320px;
+    margin: 0 auto;
+    padding: 60px 0;
+  }
 
-    .text {
-        padding: 0 30px;
-        max-width: 620px;
-        margin: 0 auto;
-    }
+  .text {
+    padding: 0 30px;
+    max-width: 620px;
+    margin: 0 auto;
+  }
 
+  .leagueData {
+    position: relative;
+    z-index: 1;
+    width: 100%;
+    min-width: 470px;
+    max-width: 470px;
+    min-height: 100%;
+    background-color: var(--ebebeb);
+    border-left: var(--eee);
+    box-shadow: inset 8px 0px 6px -6px rgb(0 0 0 / 24%);
+  }
+
+  @media (max-width: 950px) {
     .leagueData {
-        position: relative;
-        z-index: 1;
-        width: 100%;
-        min-width: 470px;
-        max-width: 470px;
-        min-height: 100%;
-		background-color: var(--ebebeb);
-        border-left: var(--eee);
-		box-shadow: inset 8px 0px 6px -6px rgb(0 0 0 / 24%);
+      max-width: 100%;
+      min-width: 100%;
+      width: 100%;
+      box-shadow: none;
     }
+    #home {
+      flex-wrap: wrap;
+    }
+  }
 
-    @media (max-width: 950px) {
-        .leagueData {
-            max-width: 100%;
-            min-width: 100%;
-            width: 100%;
-		    box-shadow: none;
-        }
-        #home {
-            flex-wrap: wrap;
-        }
-    }
+  .transactions {
+    display: block;
+    width: 95%;
+    margin: 10px auto;
+  }
 
-    .transactions {
-        display: block;
-        width: 95%;
-        margin: 10px auto;
-    }
+  .center {
+    text-align: center;
+  }
 
-    .center {
-        text-align: center;
-    }
+  h6 {
+    text-align: center;
+  }
 
-    h6 {
-        text-align: center;
-    }
+  .homeBanner {
+    background-color: var(--blueOne);
+    color: #fff;
+    padding: 0.5em 0;
+    font-weight: 500;
+    font-size: 1.5em;
+  }
 
-    .homeBanner {
-        background-color: var(--blueOne);
-        color: #fff;
-        padding: 0.5em 0;
-        font-weight: 500;
-        font-size: 1.5em;
-    }
+  /* champ styling */
+  #currentChamp {
+    padding: 25px 0;
+    background-color: var(--f3f3f3);
+    box-shadow: 5px 0 8px var(--champShadow);
+    border-left: 1px solid var(--ddd);
+  }
 
-    /* champ styling */
-    #currentChamp {
-        padding: 25px 0;
-		background-color: var(--f3f3f3);
-        box-shadow: 5px 0 8px var(--champShadow);
-        border-left: 1px solid var(--ddd);
-    }
+  #champ {
+    position: relative;
+    width: 150px;
+    height: 150px;
+    margin: 0 auto;
+    cursor: pointer;
+  }
 
-    #champ {
-        position: relative;
-        width: 150px;
-        height: 150px;
-        margin: 0 auto;
-        cursor: pointer;
-    }
+  .first {
+    position: absolute;
+    transform: translate(-50%, -50%);
+    width: 80px;
+    height: 80px;
+    border-radius: 100%;
+    border: 1px solid #ccc;
+    left: 50%;
+    top: 43%;
+  }
 
-    .first {
-        position: absolute;
-        transform: translate(-50%, -50%);
-        width: 80px;
-        height: 80px;
-        border-radius: 100%;
-        border: 1px solid #ccc;
-        left: 50%;
-        top: 43%;
-    }
+  .laurel {
+    position: absolute;
+    transform: translate(-50%, -50%);
+    width: 135px;
+    height: auto;
+    left: 50%;
+    top: 50%;
+  }
 
-    .laurel {
-        position: absolute;
-        transform: translate(-50%, -50%);
-        width: 135px;
-        height: auto;
-        left: 50%;
-        top: 50%;
-    }
+  h4 {
+    text-align: center;
+    font-size: 1.8em;
+    margin: 10px;
+    font-style: italic;
+  }
 
-    h4 {
-        text-align: center;
-        font-size: 1.8em;
-        margin: 10px;
-        font-style: italic;
-    }
+  .label {
+    display: table;
+    text-align: center;
+    line-height: 1.1em;
+    font-size: 1.7em;
+    margin: 6px auto 10px;
+    cursor: pointer;
+  }
 
-    .label {
-        display: table;
-        text-align: center;
-        line-height: 1.1em;
-        font-size: 1.7em;
-        margin: 6px auto 10px;
-        cursor: pointer;
-    }
-    
-	:global(.curOwner) {
-		font-size: 0.75em;
-		color: #bbb;
-		font-style: italic;
-	}
+  :global(.curOwner) {
+    font-size: 0.75em;
+    color: #bbb;
+    font-style: italic;
+  }
+
+  /* Countdown Timer Styles */
+  #countdown-timer {
+    font-size: 1.8em;
+    font-weight: bold;
+    color: #f5a623; /* Adjust the color to match your theme */
+    padding: 15px;
+    background-color: #00316b; /* Adjust the background color */
+    text-align: center;
+    border-radius: 5px;
+    margin-top: 20px;
+    width: 100%;
+    margin-bottom: 30px;
+  }
 </style>
 
 <div id="home">
-    <div id="main">
-        <div class="text">
-            <h6>{leagueName}</h6>
-            <!-- homepageText contains the intro text for your league, this gets edited in /src/lib/utils/leagueInfo.js -->
-            {@html homepageText }
-            <!-- Most recent Blog Post (if enabled) -->
-            {#if enableBlog}
-                <HomePost />
-            {/if}
-        </div>
-        <PowerRankings />
+  <div id="main">
+    <div class="text">
+      <h6>{leagueName}</h6>
+      <!-- homepageText contains the intro text for your league, this gets edited in /src/lib/utils/leagueInfo.js -->
+      {@html homepageText }
+
+      <!-- Countdown Timer -->
+      <div id="countdown-timer">
+        {countdownText}
+      </div>
+
+      <!-- Most recent Blog Post (if enabled) -->
+      {#if enableBlog}
+        <HomePost />
+      {/if}
     </div>
-    
-    <div class="leagueData">
-        <div class="homeBanner">
-            {#await nflState}
-                <div class="center">Retrieving NFL state...</div>
-                <LinearProgress indeterminate />
-            {:then nflStateData}
-                <div class="center">NFL {nflStateData.season} 
-                    {#if nflStateData.season_type == 'pre'}
-                        Preseason
-                    {:else if nflStateData.season_type == 'post'}
-                        Postseason
-                    {:else}
-                        Season - {nflStateData.week > 0 ? `Week ${nflStateData.week}` : "Preseason"}
-                    {/if}
-                </div>
-            {:catch error}
-                <div class="center">Something went wrong: {error.message}</div>
-            {/await}
-        </div>
+    <PowerRankings />
+  </div>
 
-        <div id="currentChamp">
-            {#await waitForAll(podiumsData, leagueTeamManagersData)}
-                <p class="center">Retrieving awards...</p>
-                <LinearProgress indeterminate />
-            {:then [podiums, leagueTeamManagers]}
-                {#if podiums[0]}
-                    <h4>{podiums[0].year} Fantasy Champ</h4>
-                    <div id="champ" onclick={() => {if(managers.length) gotoManager({year: podiums[0].year, leagueTeamManagers, rosterID: parseInt(podiums[0].champion)})}} >
-                        <img src="{getAvatarFromTeamManagers(leagueTeamManagers, podiums[0].champion, podiums[0].year)}" class="first" alt="champion" />
-                        <img src="/laurel.png" class="laurel" alt="laurel" />
-                    </div>
-                    <span class="label" onclick={() => gotoManager({year: podiums[0].year, leagueTeamManagers, rosterID: parseInt(podiums[0].champion)})} >{getTeamFromTeamManagers(leagueTeamManagers, podiums[0].champion, podiums[0].year).name}</span>
-                {:else}
-                    <p class="center">No former champs.</p>
-                {/if}
-            {:catch error}
-                <p class="center">Something went wrong: {error.message}</p>
-            {/await}
+  <div class="leagueData">
+    <div class="homeBanner">
+      {#await nflState}
+        <div class="center">Retrieving NFL state...</div>
+        <LinearProgress indeterminate />
+      {:then nflStateData}
+        <div class="center">NFL {nflStateData.season} 
+          {#if nflStateData.season_type == 'pre'}
+            Preseason
+          {:else if nflStateData.season_type == 'post'}
+            Postseason
+          {:else}
+            Season - {nflStateData.week > 0 ? `Week ${nflStateData.week}` : "Preseason"}
+          {/if}
         </div>
+      {:catch error}
+        <div class="center">Something went wrong: {error.message}</div>
+      {/await}
+    </div>
 
-        <div class="transactions" >
-            <Transactions />
+    <div id="currentChamp">
+      {#await waitForAll(podiumsData, leagueTeamManagersData)}
+        <p class="center">Retrieving awards...</p>
+        <LinearProgress indeterminate />
+      {:then [podiums, leagueTeamManagers]}
+        {#if podiums[0]}
+          <h4>{podiums[0].year} Fantasy Champ</h4>
+          <div id="champ" onclick={() => {if(managers.length) gotoManager({year: podiums[0].year, leagueTeamManagers, rosterID: parseInt(podiums[0].champion)})}} >
+            <img src="{getAvatarFromTeamManagers(leagueTeamManagers, podiums[0].champion, podiums[0].year)}" class="first" alt="champion" />
+            <img src="/laurel.png" class="laurel" alt="laurel" />
+          </div>
+          <span class="label" onclick={() => gotoManager({year: podiums[0].year, leagueTeamManagers, rosterID: parseInt(podiums[0].champion)})} >{getTeamFromTeamManagers(leagueTeamManagers, podiums[0].champion, podiums[0].year).name}</span>
+        {:else}
+          <p class="center">No former champs.</p>
+        {/if}
+      {:catch error}
+        <p class="center">Something went wrong: {error.message}</p>
+      {/await}
+    </div>
+
+    <div class="transactions">
+      <Transactions />
+    </div>
+  </div>
+</div>
+
         </div>
     </div>
 </div>
