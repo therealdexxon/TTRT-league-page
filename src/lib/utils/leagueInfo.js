c603ca06d1a42d3a6bf3c4547c1f9163657399e2
therealdexxon <95049453+therealdexxon@users.noreply.github.com>
2026-04-29 18:08:47 -0500

    Update league name for fantasy football league

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,6 +1,6 @@
 /*   STEP 1   */
 export const leagueID = "1179271510066319360"; // your league ID
-export const leagueName = "Welcome to the TTRT fantasy football league!"; // your league name
+export const leagueName = "Welcome to the TTRT Dynasty PPR fantasy football league!"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
